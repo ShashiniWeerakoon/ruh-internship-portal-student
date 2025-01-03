735a23135300201efa6f1e60d214bbcf7c0db970
IsuruSH <shanakaisuru11@gmail.com>
2025-01-03 18:45:59 +0530

    Add routing and new pages for Home and Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from "react";
 import Header from "./component/Header";
-
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
- 
-
   return (
-    <>
-      <Header/>
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    </>
-  )
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* Kamathi Routes tika dala hadaganna */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import Header from "../component/Header";
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <div>Contact</div>
+    </>
+  );
+};
+
+export default Contact;

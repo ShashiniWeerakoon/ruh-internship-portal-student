bfc3bd5c4c03124c5159fb41cf5b25c57750187e
Supipi Isanka <150982977+GSI199909@users.noreply.github.com>
2025-01-09 14:32:18 +0530

    create branh

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,15 +1,21 @@
+import React from "react";
 import Header from "../component/Header";
 import Sidebar from "../component/Sidebar";
 
 
-const Home = () => {
+const UserProfile = () => {
   return (
-    <>
+    <div className="flex flex-col h-screen">
       <Header />
-      <Sidebar />
-      <div>User Dashboard</div>
-    </>
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar />
+          <div className="flex-grow p-8 overflow-y-auto mt-16 ml-[250px]">
+            <h1 className="text-2xl font-bold mb-4">User Profile</h1>
+            
+          </div>
+        </div>
+    </div>
   );
 };
 
-export default Home;
+export default UserProfile;

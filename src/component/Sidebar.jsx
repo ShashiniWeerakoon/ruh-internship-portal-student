17abb214891c41a34a48ab5b2dc5705cb1ff6fdb
ShashiniWeerakoon <150053909+ShashiniWeerakoon@users.noreply.github.com>
2025-01-14 19:41:46 +0530

    update the sidebar

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import { FaUser, FaBook, FaBriefcase, FaClipboard, FaSortNumericDown, FaInfoCircle, FaJournalWhills, FaComments } from 'react-icons/fa';
 import styles from '../styles/sidebar.module.css';
+import { NavLink } from 'react-router-dom';
 
 export default function Sidebar() {
+  const NavLinkStyles=({ isActive}) =>{
+    return{
+      fontWeight: isActive? 'bold' :'normal',
+    }
+  }
   return (
     <div className={styles.sidebar}>
       <div className={styles.header}>Dashboard</div>
       <nav className={styles.nav}>
-        <a href="/Userprofile" className={styles['nav-item']}>
+        <NavLink style={NavLinkStyles} to ="/Userprofile" className={styles['nav-item']}>
           <FaUser className={styles['nav-icon']} /> User Profile
-        </a>
-        <a href="/Instruction" className={styles['nav-item']}>
+        </NavLink>
+        <NavLink style={NavLinkStyles} to ="/Instruction" className={styles['nav-item']}>
           <FaBook className={styles['nav-icon']} /> Instructions
-        </a>
-        <a href="/Internships" className={styles['nav-item']}>
+        </NavLink>
+        <NavLink style={NavLinkStyles} to="/Internships" className={styles['nav-item']}>
           <FaBriefcase className={styles['nav-icon']} /> Internships
-        </a>
-        <a href="/Applynow" className={styles['nav-item']}>
+        </NavLink>
+        <NavLink style={NavLinkStyles} to="/Applynow" className={styles['nav-item']}>
           <FaClipboard className={styles['nav-icon']} /> Apply Now
-        </a>
-        <a href="/Currstatus" className={styles['nav-item']}>
+        </NavLink>
+        <NavLink style={NavLinkStyles} to="/Currstatus" className={styles['nav-item']}>
           <FaSortNumericDown className={styles['nav-icon']} /> Current Status
-        </a>
-        <a href="/About" className={styles['nav-item']}>
+        </NavLink>
+        <NavLink style={NavLinkStyles} to="/About" className={styles['nav-item']}>
           <FaInfoCircle className={styles['nav-icon']} /> About Internship
-        </a>
-        <a href="/Diary" className={styles['nav-item']}>
+        </NavLink>
+        <NavLink style={NavLinkStyles} to="/Diary" className={styles['nav-item']}>
           <FaJournalWhills className={styles['nav-icon']} /> Diary Updates
-        </a>
-        <a href="/Feedback" className={styles['nav-item']}>
+        </NavLink>
+        <NavLink style={NavLinkStyles} to="/Feedback" className={styles['nav-item']}>
           <FaComments className={styles['nav-icon']} /> Feedbacks
-        </a>
+        </NavLink>
       </nav>
     </div>
   );
